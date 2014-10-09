a0dc2b91be4c3dc6e943163594c78b9c6ae32799
Grinch <artem@zappylab.com>
2014-10-10 00:03:39 +0400

    Signed-off-by: Grinch <artem@zappylab.com>

--- src/main/java/application/Controller.java
+++ src/main/java/application/Controller.java
@@ -1,5 +1,15 @@
 package application;
 
+import org.jcodec.api.FrameGrab;
+import org.jcodec.api.JCodecException;
+import org.jcodec.common.NIOUtils;
+import org.jcodec.common.SeekableByteChannel;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * Author Grinch
  * Date: 07.10.14
@@ -15,8 +25,36 @@ public class Controller {
     private Controller() {
     }
 
-    public static void main(String[] args) {
-        MainWindow main = new MainWindow();
-        main.setVisible(true);
+    public static void main(String[] args){
+        //MainWindow main = new MainWindow();
+        //main.setVisible(true);
+        int i = 0;
+        long startTime = System.currentTimeMillis();
+        try {
+            SeekableByteChannel sbc = NIOUtils.readableFileChannel(new File("src/main/resources/cars.mp4"));
+            FrameGrab grab = new FrameGrab(sbc);
+           // grab.seek(startSec);
+            BufferedImage frame;
+            for (; true; i++) {
+                try{
+                    frame = grab.getFrame();
+                }catch (Exception e){
+                    System.out.println("Frames are out");
+                    break;
+                }
+
+                //ImageIO.write(frame, "png",
+                //        new File(String.format("src/main/resources/frames/frame_%08d.png", i)));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (JCodecException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long endTime   = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println(totalTime/1000. +"s "+i+ " frames");
     }
 }
--- src/main/java/application/MainWindow.java
+++ src/main/java/application/MainWindow.java
@@ -1,9 +1,21 @@
 package application;
 
+import javax.swing.*;
+
 /**
  * Author Grinch
  * Date: 07.10.14
  * Time: 22:20
  */
-public class MainWindow {
+public class MainWindow extends JFrame {
+    private JPanel panel1;
+
+    public MainWindow(){
+        super("GEOClient:Connect");
+        //setIconImage(new ImageIcon("res/icon.gif").getImage());
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        getContentPane().add(panel1);
+        pack();
+        setLocationRelativeTo(null);
+    }
 }
